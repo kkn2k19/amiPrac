1ab40964a537cf0347bae274bdf4541420b09fc2
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2025-09-30 01:55:32 +0530

    .

---
+++ C_Basics/printfDemo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World!\n");
+    return 0;
+}
+
+// E:\Karan_codes\amiPrac>cd "e:\Karan_codes\amiPrac\C_Basics\" && gcc printfDemo.c -o printfDemo && "e:\Karan_codes\amiPrac\C_Basics\"\printfDemo && del printfDemo.exe
+// Hello, World!
